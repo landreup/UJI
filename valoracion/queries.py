848249864d8767653fe007bdfe7a862d63b8e202
luis <landreup@gmail.com>
2012-06-05 11:10:36 +0200

    Arreglado boton ir

--- valoracion/queries.py
+++ valoracion/queries.py
@@ -140,7 +140,7 @@ class NodeEvaluation(NodeEvaluation):
     
     def getForm(self):
         if self.status == "unlock" :
-            if self.evaluation.getEvaluador() == self.rol:
+            if self.evaluation.getEvaluator() == self.rol:
                 return self.evaluationForm.formulario.codigo
         return None
     
