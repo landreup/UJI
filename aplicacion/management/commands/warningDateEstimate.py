1ad7a4952059e2b54e65b6677f193dedd56850d9
luis <landreup@gmail.com>
2012-06-05 13:10:41 +0200

    Creado script de pruebas para el envio de avisos

--- aplicacion/management/commands/warningDateEstimate.py
+++ aplicacion/management/commands/warningDateEstimate.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 import datetime
 from proyecto.queries import QueryEstimateDate, QueryProject
 from valoracion.queries import QueryForm
+from settings import SERVER_NAME
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -27,7 +28,7 @@ class Command(BaseCommand):
                 email.subject = u"Valoració del " + unicode(item).lower() + " del alumne " + unicode(project.alumno.nombreCompleto())
                 email.from_email = 'UJI - Evaluació d\'estudiants de projecte Fi de Grau<provauji@gmail.com>'
                 email.to = ['landreup@gmail.com']
-                email.body = "Alumno: " + unicode(project.alumno) + "\nResponsables formulario: " + cadena + "\nROL:" +rol
+                email.body = "Alumno: " + unicode(project.alumno) + "\nResponsables formulario: " + cadena + "\nROL:" +rol + "\n"+ SERVER_NAME
                 email.send()
                 print "Se ha enviado un mail"
 
--- settings.py
+++ settings.py
@@ -10,6 +10,8 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+SERVER_NAME = "eros.act.uji.es"
+
 MANAGERS = ADMINS
 
 DATABASES = {
