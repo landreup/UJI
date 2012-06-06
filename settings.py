9866072e4f3060754b8dc57c9402bdfa0799b70a
luis <landreup@gmail.com>
2012-06-06 14:04:18 +0200

    Problemas con la indicacion de errores en proyectos bloqueados

--- settings.py
+++ settings.py
@@ -20,14 +20,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'evalua',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'Vilafames',                  # Not used with sqlite3.
+        'PASSWORD': 'vilafames',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'provauji@gmail.com'
-EMAIL_HOST_PASSWORD = 'vilafames'
+EMAIL_HOST_PASSWORD = 'Vilafames'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # Local time zone for this installation. Choices can be found here:
