7d4883df17ab92494c1457477c702775faca3c89
luis <landreup@gmail.com>
2012-06-07 15:55:30 +0200

    Fecha tope en listado proyectos

--- curso/models.py
+++ curso/models.py
@@ -5,5 +5,8 @@ class Curso(models.Model):
     fechaTope = models.DateField(null=True, blank=True)
     esActual = False
     
+    def getFechaTope(self):
+        return self.fechaTope
+    
     def __unicode__(self):
         return unicode(self.curso) + "/" + unicode(self.curso+1)
